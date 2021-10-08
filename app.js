ac0ecd04f575f8245d938d627d7058106f919093
Lorenzo Epifani <lor.epifani@stud.uniroma3.it>
2021-10-08 23:13:50 +0200

    toggle grid outline

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const DEFAULT_SIZE = 16; // 16x16 is default size
 const grid = document.getElementById("grid");
-let chosenColor = "black";
+const inputColor = document.getElementById("color-picker");
+let currentColor = inputColor.value
+const checkBox = document.getElementById("check");
+let cells;
+let gridOutline = true;
 
 function cellHover(e) {
-    e.target.style.backgroundColor = chosenColor;
+    e.target.style.backgroundColor = currentColor;
 }
 
 function cellHoverEnd(e) {
-    e.target.style.backgroundColor = chosenColor;
+    e.target.style.backgroundColor = currentColor;
 }
 
 function createGrid(numberOfRows = DEFAULT_SIZE, numberOfColumns = DEFAULT_SIZE) {
@@ -22,6 +26,30 @@ function createGrid(numberOfRows = DEFAULT_SIZE, numberOfColumns = DEFAULT_SIZE)
             grid.appendChild(cell);
         }
     }
+    cells = Array.from(document.querySelectorAll("#grid div"));
+}
+
+function changeGridOutline() {
+    if(gridOutline) {
+        cells.forEach(cell => {
+            cell.style.border = "none";
+        });
+        gridOutline = false;
+    }
+    else {
+        cells.forEach(cell => {
+            cell.style.border = "0.1rem solid #7F7F7F"
+        });
+        gridOutline = true;
+    }
+}
+
+function changeCurrentColor(e) {
+    currentColor = e.target.value;
 }
 
 createGrid();
+
+inputColor.addEventListener("change", changeCurrentColor);
+
+checkBox.addEventListener("change", changeGridOutline);
